7bd991c32f59c11a74161da0012b0b77cebc8b09
Mathis Dirksen-Thedens <info@zephyrsoft.org>
2023-09-10 08:43:22 +0200

    handle Umlauts more gracefully

--- src/main/java/org/zephyrsoft/optigemspoonfeeder/model/SearchableString.java
+++ src/main/java/org/zephyrsoft/optigemspoonfeeder/model/SearchableString.java
@@ -1,5 +1,11 @@
 package org.zephyrsoft.optigemspoonfeeder.model;
 
+import static java.util.stream.Collectors.toSet;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class SearchableString {
 	private final String value;
 	private final String valueLowercase;
@@ -13,8 +19,23 @@ public class SearchableString {
 		if (valueLowercase == null) {
 			return false;
 		}
-		for (String term : terms) {
-			if (valueLowercase.contains(term.toLowerCase())) {
+
+		Set<String> termsToApply = Arrays.stream(terms)
+				.map(String::toLowerCase)
+				.collect(toSet());
+
+		// MT940 file and Hibiscus Server import use different Umlaut strategies,
+		// so check both
+		Set<String> additionalTermsToApply = new HashSet<>();
+		for (String term : termsToApply) {
+			if (term.contains("ae") || term.contains("oe") || term.contains("ue")) {
+				additionalTermsToApply.add(term.replace("ae", "ä").replace("oe", "ö").replace("ue", "ü"));
+			}
+		}
+		termsToApply.addAll(additionalTermsToApply);
+
+		for (String term : termsToApply) {
+			if (valueLowercase.contains(term)) {
 				return true;
 			}
 		}
