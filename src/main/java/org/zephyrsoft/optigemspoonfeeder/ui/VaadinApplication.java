d7bad7ac7a2df3b4671c8408363eb775f504def3
Mathis Dirksen-Thedens <info@zephyrsoft.org>
2025-12-04 18:32:33 +0100

    remove deprecated Theme annotation and reference CSS file directly

--- src/main/java/org/zephyrsoft/optigemspoonfeeder/ui/VaadinApplication.java
+++ src/main/java/org/zephyrsoft/optigemspoonfeeder/ui/VaadinApplication.java
@@ -1,11 +1,11 @@
 package org.zephyrsoft.optigemspoonfeeder.ui;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.shared.communication.PushMode;
-import com.vaadin.flow.theme.Theme;
 
-@Theme("custom-theme")
+@StyleSheet("styles.css")
 @Push(PushMode.DISABLED)
 public class VaadinApplication implements AppShellConfigurator {
 
