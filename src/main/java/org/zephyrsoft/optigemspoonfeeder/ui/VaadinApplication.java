ec660ed957f3c7c682cd819960fd572be5cb8a24
Mathis Dirksen-Thedens <info@zephyrsoft.org>
2025-12-05 17:42:58 +0100

    explicitly import theme Lumo

--- src/main/java/org/zephyrsoft/optigemspoonfeeder/ui/VaadinApplication.java
+++ src/main/java/org/zephyrsoft/optigemspoonfeeder/ui/VaadinApplication.java
@@ -4,7 +4,9 @@ import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.shared.communication.PushMode;
+import com.vaadin.flow.theme.lumo.Lumo;
 
+@StyleSheet(Lumo.STYLESHEET)
 @StyleSheet("styles.css")
 @Push(PushMode.DISABLED)
 public class VaadinApplication implements AppShellConfigurator {
